000281f02df520612e3722cf274d700392518350
Fernando Martin Garcia Del Angel <a01334390@itesm.mx>
2019-05-11 00:54:18 -0500

    Added Mouse support

---
+++ Mouse.cpp
@@ -0,0 +1,16 @@
+#include "Mouse.hpp"
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
+Mouse::Mouse(){
+    x = 0;
+    y = 0;
+    lmb = 0;
+    mmb = 0;
+    rmb = 0;
+    xpress = 0;
+    ypress = 0;
+};
+
+Mouse::~Mouse(){
+    
+};
---
+++ main.cpp
@@ -0,0 +1,35 @@
+// Please don't change lines 9-31 (It helps me to grade)
+#ifdef __APPLE__
+// For XCode only: New C++ terminal project. Build phases->Compile with libraries: add OpenGL and GLUT
+// Import this whole code into a new C++ file (main.cpp, for example). Then run.
+// Reference: https://en.wikibooks.org/wiki/OpenGL_Programming/Installation/Mac
+	#include <OpenGL/gl.h>
+	#include <OpenGL/glu.h>
+	#include <GLUT/glut.h>
+#endif
+#ifdef _WIN32
+// For VS on Windows only: Download CG_Demo.zip. UNZIP FIRST. Double click on CG_Demo/CG_Demo.sln
+// Run
+	#include "freeglut.h"
+#endif
+#ifdef __unix__
+// For Linux users only: g++ CG_Demo.cpp -lglut -lGL -o CG_Demo
+// ./CG_Demo
+// Reference: https://www.linuxjournal.com/content/introduction-opengl-programming
+	#include "GL/freeglut.h"
+	#include "GL/gl.h"
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+/** Include handmade Classes */
+#include "Mouse.hpp"
+
+/** Create a global mouse */
+Mouse* MickeyMouse;
+
+/** Window size */
+int winw = 640;
+int winh = 480;
